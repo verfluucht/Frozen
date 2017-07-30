7ca7ab9f361d62e708fa5b573868d45c7c0e0ed1
WiNiFiX <30388134+winifix@users.noreply.github.com>
2017-07-30 14:50:41 +0200

    Fix Loading bug when not in OrderHall

--- LUA/UI.lua
+++ LUA/UI.lua
@@ -48,19 +48,5 @@ end
 
 button:SetScript("OnClick", startStop)
 
--- License: Public Domain - HideOderHallBar
-local b = OrderHallCommandBar
-b:SetScript("OnShow", b.Hide)
-b:Hide()
-
--- initial call for other addons, re-register events
-b:RequestCategoryInfo()
-b:RegisterEvent("GARRISON_TALENT_COMPLETE")
-b:RegisterEvent("GARRISON_TALENT_UPDATE")
-b:RegisterUnitEvent("UNIT_PHASE", "player")
-
--- prevent buffs jumping up/down
-UIParent:UnregisterEvent("UNIT_AURA")
-
 -- Hide the annoying quest tracker frame 
 --ObjectiveTrackerFrame:Hide()
